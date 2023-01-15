c3dd9d80ef205adc3ec0fb906d0f551cc4398a01
taibislamdipu <mailtaibislam@gmail.com>
2023-01-16 04:27:41 +0600

    updated

--- src/components/SuggestedHotel/SuggestedHotelCard.jsx
+++ src/components/SuggestedHotel/SuggestedHotelCard.jsx
@@ -3,11 +3,17 @@
 
 import React from "react";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import { IoLocationSharp } from "react-icons/io5";
 import hotelImgSmall from "../../assets/images/hotelImgSmall.jpg";
 
-const SuggestedHotelCard = ({ hotel_name, address, price }) => {
+const SuggestedHotelCard = ({
+  hotel_name,
+  address,
+  price,
+  isTabletOrMobile,
+}) => {
   return (
-    <section className="grid grid-cols-8 gap-3 border hover:shadow-lg transition p-3">
+    <section className="grid grid-cols-8 gap-3 mb-2 border hover:shadow-lg transition p-3">
       <div className="col-span-2">
         <div>
           <img className="max-h-44 w-full" src={hotelImgSmall} alt="" />
@@ -15,32 +21,57 @@ const SuggestedHotelCard = ({ hotel_name, address, price }) => {
       </div>
       <div className="col-span-4 space-y-3 border-r">
         <h3 className="text-base font-semibold">{hotel_name}</h3>
-        <p>{address} | Map</p>
-        <ul className="space-y-1">
-          <li className="flex items-center gap-1">
+
+        {isTabletOrMobile ? (
+          <span className="flex gap-1">
             <BsFillInfoCircleFill />
-            Swimming Pool
-          </li>
-          <li className="flex items-center gap-1">
             <BsFillInfoCircleFill />
-            Free Airport Shuttle
-          </li>
-          <li className="flex items-center gap-1">
             <BsFillInfoCircleFill />
-            Pets Allowed
-          </li>
-        </ul>
+          </span>
+        ) : (
+          <>
+            <p>{address} | Map</p>
+            <ul className="space-y-1">
+              <li className="flex items-center gap-1">
+                <BsFillInfoCircleFill />
+                Swimming Pool
+              </li>
+              <li className="flex items-center gap-1">
+                <BsFillInfoCircleFill />
+                Free Airport Shuttle
+              </li>
+              <li className="flex items-center gap-1">
+                <BsFillInfoCircleFill />
+                Pets Allowed
+              </li>
+            </ul>
+          </>
+        )}
       </div>
+
       <div className="col-span-2 space-y-2 text-center">
         <div>
-          <h2 className="text-4xl font-bold">${price}</h2>
+          <h2 className="lg:text-4xl text-2xl font-bold">${price}</h2>
           <p>
             <small>per night</small>
           </p>
         </div>
-        <button className="btn">Book Now</button>
-        <p>View Available Rooms</p>
+        {!isTabletOrMobile && (
+          <>
+            <button className="btn">Book Now</button>
+            <p>View Available Rooms</p>
+          </>
+        )}
       </div>
+
+      {isTabletOrMobile && (
+        <p className="col-span-8 flex items-center">
+          <span>
+            <IoLocationSharp size={23} />
+          </span>
+          Dhaka, Bangladesh
+        </p>
+      )}
     </section>
   );
 };
