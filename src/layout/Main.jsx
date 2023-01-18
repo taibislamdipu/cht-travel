c239f76f84b429d93e2c6d02325d48cca7a3c374
taibislamdipu <mailtaibislam@gmail.com>
2023-01-18 14:11:53 +0600

    scroll to top addedsd

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import ScrollToTopComponent from "../components/ScrollToTopComponent/ScrollToTopComponent";
 import Navbar from "./Navbar";
 
 const Main = () => {
   return (
     <div className="lg:mx-40 px-4">
-      <Navbar />
-      <Outlet />
+      <ScrollToTopComponent>
+        <Navbar />
+        <Outlet />
+      </ScrollToTopComponent>
     </div>
   );
 };
