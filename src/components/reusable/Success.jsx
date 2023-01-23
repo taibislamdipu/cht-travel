e45074751fc6c96c84a1c8e6f939046ff85f06d2
Tamanna14-cpu <bimurto.bithy@gmail.com>
2023-01-23 23:15:24 +0600

    success img added

--- src/components/reusable/Success.jsx
+++ src/components/reusable/Success.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import paymentSuccess from "../../assets/images/payment.gif";
 
 const Success = () => {
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Your order confirm Successfully</h1>
+    <div className="flex justify-between items-center customContainer">
+      <h1 className="text-4xl font-bold text-black">
+        Your order confirm Successfully!
+      </h1>
+      <div>
+        <img src={paymentSuccess} alt="success" />
+      </div>
     </div>
   );
 };
