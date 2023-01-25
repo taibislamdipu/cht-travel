0b8abd807b659bd9b3908dd3b3e31877e56860f6
taibislamdipu <mailtaibislam@gmail.com>
2023-01-26 04:36:21 +0600

    all changes done

--- src/layout/Dashboard/Sidebar.jsx
+++ src/layout/Dashboard/Sidebar.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {
-  AiFillFileAdd,
-  AiFillHome,
-  AiOutlineFileAdd,
-  AiOutlineUnorderedList,
-} from "react-icons/ai";
-import { FcAddDatabase } from "react-icons/fc";
-import { HiOutlineViewGridAdd } from "react-icons/hi";
+import { AiFillFileAdd, AiFillHome } from "react-icons/ai";
 import { IoPersonAddSharp } from "react-icons/io5";
 import { BsList } from "react-icons/bs";
 
 const Sidebar = () => {
   return (
-    <div className="col-span-2 md:h-[calc(100vh-25px)] rounded-lg bg-[#212D3A] text-white p-5">
-      <ul className="flex h-full  flex-col gap-3">
+    <div className="col-span-2 md:h-[calc(100vh-25px)] rounded-lg bg-[#212D3A] text-white p-5 dashboard-sidebar">
+      <ul className="flex h-full flex-col gap-3">
         <div>
-          <li className="font-bold ">CHT Admin Dashboard</li>
+          <li className="font-bold ">
+            <Link to="/dashboard" className="hover:underline">
+              CHT Admin Dashboard
+            </Link>
+          </li>
           <code>version: 1.0.1</code>
         </div>
 
